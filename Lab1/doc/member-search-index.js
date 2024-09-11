2327de5610dbcf82c9640be2310dfd148467cc81
Tanya <vorobest5@gmail.com>
2024-09-11 22:08:14 +0300

    add Lab2

--- Lab1/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1VorobetsKI305","l":"Lab1VorobetsKI305()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1VorobetsKI305","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
