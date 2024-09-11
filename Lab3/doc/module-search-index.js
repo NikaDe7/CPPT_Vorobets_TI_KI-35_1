69b1dd5195f1e8dad3dcd6371974cbe99d1e710f
Tanya <vorobest5@gmail.com>
2024-09-11 22:10:46 +0300

    add Lab4

--- Lab3/doc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"Lab3"}];updateSearchResults();
--- Lab3/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Lab3","l":"KI_305.Vorobets.Lab3"}];updateSearchResults();
